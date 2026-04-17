7d73d0b1811722a1051ebc5d9cd0cb0ad01a6442
huypham1106 <huy01294724088@gmail.com>
2026-04-17 21:42:37 +0700

    add: new script

---
+++ Assets/A_Script/Huy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public enum Direction
+{
+    North,
+    South,
+    East,
+    West
+}
+
+public enum DiagonalDirection
+{
+    up,
+    upright,
+    right,
+    downright,
+    down,
+    downleft,
+    left,
+    upleft
+}
+
+
+//public enum TerrainType
+//{
+//    DeepWater,
+//    ShallowWater,
+//    Sand,
+//    Grass,
+//    Rock
+//}
